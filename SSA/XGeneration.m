799cb83c0ccc5cc2c9369946af9a64521632b3e1
Synapt1x <chriscadonic@gmail.com>
2015-05-26 13:22:25 -0500

    Add changes to XGeneration.m

--- SSA/XGeneration.m
+++ SSA/XGeneration.m
@@ -1,10 +1,11 @@
-function [X0] = XGeneration(Old_X, aj, V, num_species, tau)
-    X0=[]; % make a blank vector to store the new amounts of all three substances
-    for n=1 :num_species % loop through changes in all species from all reactions
-        aspec = aj(n); % current a value of a substance
-        vspec = V(n); % changes in amounts of substances from each reaction
-        poi_ran = poissrnd(tau*aspec); % generate a poisson random number
-        change_amount = poi_ran * vspec; % the change in the substance is the # of reactions * the amount used in each reactin
-        final_amount = change_amount + Old_X(n); % add change to previous amount to get new amount
-        X0 = [X0 final_amount]; % append new value for one substance to other substances 
+function X_new = XGeneration(Old_X, aj, V, num_rx, tau)
+    X_new=Old_X; % make a blank vector to store the new amounts of all three substances
+    for n=1 :num_rx % loop through changes in all species from all reactions
+        arx = aj(n); % current a value of a substance
+        vrx = V(n,:); % changes in amounts of substances from each reaction
+        poi_ran = poissrnd(tau*arx); % generate a poisson random number
+        change_amount = poi_ran * vrx; % the change in the substance is the # of reactions * the amount used in each reactin
+        X_new = X_new + change_amount; % add change to previous amount to get new amount
     end
+    
+   
